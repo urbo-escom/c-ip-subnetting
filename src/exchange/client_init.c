2c93c3a9a28a9d1fb4000b5940aa150d973c96a0
urbo <urbo.g75@gmail.com>
2013-11-05 01:05:43 -0600

    Agregado implementación de intercambio (cliente)

---
+++ src/exchange/client_init.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <gtk/gtk.h>
+
+#include "bsd-win-sock.h"
+#include "exchange.h"
+#include "debug.h"
+
+extern int  Client_init(
+		Client_T* sock,
+		int (*request)(
+			char* request, const char* response, int size)) {
+	if (Socket_connect(sock) < 0) {
+		log_err("Socket connect failed: %s", Socket_strerror());
+		return -1;
+	}
+
+	Socket_timeout(sock, 5);
+	char request_s[65537];
+	char response[65537];
+	int bytes_recv;
+	int bytes_sent;
+	int status;
+	memset(response, 0, sizeof(response));
+	do {
+		log_info("Server: %s:%s",
+		         Socket_get_ip(sock), Socket_get_port(sock));
+
+		status = request(request_s, response, 65537);
+
+		bytes_sent = Socket_send(sock, request_s, strlen(request_s));
+		if (bytes_sent == -1)
+			log_warn("Error at sending request");
+
+		bytes_recv = Socket_receive(sock, response, 65537);
+		if (bytes_recv == -1) {
+			log_warn("Error at receiving response");
+		} else {
+			log_debug("Client received '%.*s'", bytes_recv,
+			          response);
+		}
+	} while(status == 0);
+	return 0;
+}
+
